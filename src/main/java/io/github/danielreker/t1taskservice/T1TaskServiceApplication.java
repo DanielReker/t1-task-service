a6064b81e4afac1ec1b546ea1b6a7fc16d97af07
DanielReker <sasha29072004@gmail.com>
2025-06-24 23:58:07 +0400

    Init

---
+++ src/main/java/io/github/danielreker/t1taskservice/T1TaskServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.danielreker.t1taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1TaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1TaskServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/danielreker/t1taskservice/T1TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielreker.t1taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1TaskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
